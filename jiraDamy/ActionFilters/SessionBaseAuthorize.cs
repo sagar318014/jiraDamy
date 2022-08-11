a9ba5aa3997eb5ebf032d911aeafc37a8ed1afd4
gavand-sagar <gavand.sagar.s@gmail.com>
2022-08-12 00:57:03 +0530

    authentication

---
+++ jiraDamy/ActionFilters/SessionBaseAuthorize.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace jiraDamy.ActionFilters
+{
+    public class SessionBaseAuthorize : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (((Controller)filterContext.Controller).Session["Username"] == null)
+            {
+                filterContext.Result = new RedirectResult("/Authentication/Login");
+            }
+        }
+    }
+}
