87c55c01ba6a032ae7eaa493a701f7aa1017f418
shreyasgavand9711 <shreyasgavand9711@gmail.com>
2022-08-24 16:57:35 +0530

    tostring

---
+++ jiraDamy/Controllers/BaseController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jiraDamy.Controllers
+{
+    public class BaseController : Controller
+    {
+        public string RenderPartialToString(string viewName, object model)
+        {
+            ControllerContext ControllerContext = this.ControllerContext;
+            if (string.IsNullOrEmpty(viewName))
+                viewName = ControllerContext.RouteData.GetRequiredString("action");
+            ViewDataDictionary ViewData = new ViewDataDictionary();
+            TempDataDictionary TempData = new TempDataDictionary();
+            ViewData.Model = model;
+
+            using (StringWriter sw = new StringWriter())
+            {
+                ViewEngineResult viewResult = ViewEngines.Engines.FindPartialView(ControllerContext, viewName);
+                ViewContext viewContext = new ViewContext(ControllerContext, viewResult.View, ViewData, TempData, sw);
+                viewResult.View.Render(viewContext, sw);
+
+                return sw.GetStringBuilder().ToString();
+            }
+
+        }
+    }
+}
